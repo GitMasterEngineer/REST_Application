4abc82c1a27e86daa5248237eebbfb949b2bba73
GitMasterEngineer <anuppochchhi25@gmail.com>
2025-03-24 02:39:03 +0530

    Third Commit

--- src/main/java/com/blogApp/Blog_application_project/controllers/PostController.java
+++ src/main/java/com/blogApp/Blog_application_project/controllers/PostController.java
@@ -1,10 +1,14 @@
 package com.blogApp.Blog_application_project.controllers;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,12 +19,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.blogApp.Blog_application_project.config.AppConstant;
 import com.blogApp.Blog_application_project.dto.ApiResponse;
 import com.blogApp.Blog_application_project.dto.PostDto;
 import com.blogApp.Blog_application_project.dto.PostResponse;
+import com.blogApp.Blog_application_project.service.FileService;
 import com.blogApp.Blog_application_project.service.PostService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/api/")
@@ -28,6 +36,12 @@ public class PostController {
 
 	@Autowired
 	private PostService postService;
+
+	@Autowired
+	private FileService fileService;
+
+	@Value("${project.image}")
+	private String path;
 
 	@PostMapping("/user/{userId}/category/{categoryId}/posts")
 	public ResponseEntity<PostDto> createPost(@RequestBody PostDto postdto, @PathVariable Integer userId,
@@ -91,4 +105,31 @@ public class PostController {
 		List<PostDto> posts = this.postService.searchPosts(keyword);
 		return new ResponseEntity<>(posts, HttpStatus.OK);
 	}
+
+	// post image upload
+	@PostMapping("/post/image/upload/{postId}")
+	public ResponseEntity<PostDto> uploadPostImage(@RequestParam("image") MultipartFile image,
+			@PathVariable Integer postId) throws IOException {
+		PostDto postDto = this.postService.getPostById(postId);
+		String fileName = this.fileService.uploadImage(path, image);
+
+		postDto.setImageName(fileName);
+		PostDto updatePost = this.postService.updatePost(postDto, postId);
+		return new ResponseEntity<PostDto>(updatePost, HttpStatus.OK);
+	}
+
+	// method to serve files
+	@GetMapping(value ="/post/image/{imageName}",produces = MediaType.IMAGE_JPEG_VALUE)
+	public void downloadImage(@PathVariable("imageName") String imageName, HttpServletResponse response) throws IOException {
+		try {
+		InputStream resource = this.fileService.getResource(path, imageName);
+		response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+		org.springframework.util.StreamUtils.copy(resource, response.getOutputStream());
+		}catch(FileNotFoundException ex)
+		{
+			System.out.println("Image Not Found Exception:"+imageName);
+			response.sendError(HttpServletResponse.SC_NOT_FOUND,"Image Not Found");
+		}
+	}
+
 }
